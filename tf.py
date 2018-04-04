e8143f04d77d9ebc31ae3e3e9bf7a4a253374bf3
touchylk <weikuangli@163.com>
2018-04-04 20:08:44 +0800

    show mnist

--- tf.py
+++ tf.py
@@ -1,3 +1,11 @@
 from __future__ import print_function
+from tensorflow.examples.tutorials.mnist import input_data
+import cv2
 import tensorflow as tf
-print(tf.__version__)
+mnist = input_data.read_data_sets("/home/kuang/dataset/",one_hot=True)
+print(mnist.train.images.shape,mnist.train.labels.shape)
+print(mnist.test.images.shape,mnist.test.labels.shape)
+print(mnist.validation.images.shape,mnist.validation.labels.shape)
+
+cv2.imshow('a',(mnist.train.images[133]).reshape([28,28]))
+cv2.waitKey(0)
