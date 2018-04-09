af948d017491f594c95602f6e0a2cb5497452773
touchylk <weikuangli@163.com>
2018-04-09 10:03:12 +0800

    mnistsoftmax完成

--- tf.py
+++ tf.py
@@ -3,25 +3,25 @@ from tensorflow.examples.tutorials.mnist import input_data
 import cv2
 import numpy as np
 import tensorflow as tf
-mnist = input_data.read_data_sets("/home/kuang/dataset/",one_hot=True)
+mnist = input_data.read_data_sets("/media/kuang/D/dataset/mnist",one_hot=True)
 x = tf.placeholder(tf.float32,[None,784])
 w = tf.Variable(tf.zeros([784,10]))
 b = tf.Variable(tf.zeros([10]))
 y = tf.nn.softmax(tf.matmul(x,w)+b)
 y_ = tf.placeholder(tf.float32,[None,10])
-loss = tf.reduce_mean(-tf.reduce_sum(y_*tf.log(y)))
+loss = tf.reduce_mean(tf.nn.softmax_cross_entropy_with_logits(labels=y_,logits=y))
 train_step = tf.train.GradientDescentOptimizer(0.5).minimize(loss)
 sess = tf.InteractiveSession()
 tf.global_variables_initializer().run()
 
-for i in range(1000):
+for i in range(100):
     b_x ,b_y = mnist.train.next_batch(100)
     feed_dict = {x:b_x,y_:b_y}
-    train_step.run(feed_dict)
-y2= tf.nn.softmax(tf.matmul((mnist.train.images[13432]).reshape([1,784]), w)+b)
+    sess.run(train_step,feed_dict=feed_dict)
+correct_predection = tf.equal(tf.argmax(y,1),tf.argmax(y_,1))
+s = tf.reduce_mean(tf.cast(correct_predection,tf.float32))
+
+print(sess.run(s,feed_dict={x:mnist.test.images,y_:mnist.test.labels}))
 
 
 
-print(sess.run(y2))
-cv2.imshow('a',(mnist.train.images[13432]).reshape([28,28]))
-cv2.waitKey(0)
